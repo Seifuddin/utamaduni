4b849c83d9f84b2c71293d8d4eec53f550af468a
Seifuddin <nguruedwin46@gmail.com>
2025-11-28 19:45:21 +0300

    added sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://utamaduniorganization.vercel.app',
-  generateRobotsTxt: true, // (optional) generate robots.txt
-  sitemapSize: 7000,       // maximum URLs per sitemap file
+  generateRobotsTxt: true, // creates robots.txt automatically
   changefreq: 'weekly',
   priority: 0.7,
+  sitemapSize: 5000
 };
